aacac8b39acfd013ac9b7c77080f56ef09420828
aomkirby123 <aomkirby123@gmail.com>
2019-05-26 12:12:41 +0700

    Add image endpoint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+const Axios = require("axios");
 
 const getGenerate = async id => {
     let returnData;
@@ -64,12 +64,42 @@ const getTag = async (tag, page = 1) => {
     return returnData;
 };
 
+const getImage = async (id, page) => {
+    let returnData, fetchPromise;
+    if(page === undefined){
+        fetchPromise = new Promise((resolve,reject) => {
+            console.log(`https://opener.now.sh/api/image/${id}`);
+            Axios(`https://opener.now.sh/api/image/${id}`).then(data => {
+                resolve(true);
+                return returnData = data.data;
+            }).catch(err => {
+                resolve(false);
+                return returnData = err.data;
+            });
+        });
+    } else {
+        fetchPromise = new Promise((resolve,reject) => {
+            Axios(`https://opener.now.sh/api/image/${id}/${page}`).then(data => {
+                resolve(true);
+                return returnData = data.data;
+            }).catch(err => {
+                resolve(false);
+                return returnData = err.data;
+            });
+        });
+    }
+    
+    await fetchPromise
+    return returnData;
+};
+
 const OpenerAPI = {
     getOpenGraph: id => getOpenGraph(id),
     getGenerate: id => getGenerate(id),
     getRelate: id => getRelate(id),
     getData: id => getData(id),
-    getTag: (tag,page = 1) => getTag(tag,page)
+    getTag: (tag, page = 1) => getTag(tag, page),
+    getImage: (id, page) => getImage(id, page)
 };
 
 module.exports = OpenerAPI;
